1c53c863788957cc322f5cfae57fffbce36c7cfc
Christian Rose <info@chrose.de>
2017-10-27 20:56:39 +0200

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 cd /etc/nginx/sites-enabled
 for file in *;
 do
-if [ ! -d /var/www/$file ] then
+if [ ! -d /var/www/$file ]; then
   mkdir -p /var/www/$file
 fi
 certbot certonly --webroot -w /var/www/$file -d $file
