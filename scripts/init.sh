3ee69403d8b29cd6a9729f2fb46136b5a4b6a96e
Christian Rose <info@chrose.de>
2017-10-27 20:47:33 +0200

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 cd /etc/nginx/sites-enabled
 for file in *;
-do 
+do
+if [ ! -d /var/www/$file ] then
+  mkdir -p /var/www/$file
+fi
 certbot certonly --webroot -w /var/www/$file -d $file
 done
 nginx -g daemon off;
