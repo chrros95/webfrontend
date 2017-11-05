9d57627ca8015c45df09ddb178fdeaded2b28cfd
Christian Rose <info@chrose.de>
2017-11-05 13:25:54 +0100

    Moved file-basename

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 for file in /etc/nginx/sites-enabled/*;
 do
-if [ ! -d /var/www/$file ]; then
-  mkdir -p /var/www/$file
-fi
 domain=$(basename $file)
 echo $domain
+if [ ! -d /var/www/$domain ]; then
+  mkdir -p /var/www/$domain
+fi
 certbot certonly --standalone -w /var/www/$domain -d $domain
 echo "0 0 * */2 * certbot renew --webroot -w /var/www/$domain -d $domain" >> mycron
 done
