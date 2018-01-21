b4d84391f148f8313a0e4fefd4ce4118af38933c
Christian Rose <info@chrose.de>
2018-01-21 18:44:20 +0100

    Changed cronjob
    Changed renew to certonly as renew only supports update of all certs

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,7 +7,7 @@ if [ ! -d /var/www/$domain ]; then
   mkdir -p /var/www/$domain
 fi
 certbot certonly --standalone -w /var/www/$domain -d $domain
-echo "0 0 * */2 * certbot renew --webroot -w /var/www/$domain -d $domain" >> mycron
+echo "0 0 * */2 * certbot certonly --webroot -w /var/www/$domain -d $domain" >> mycron
 done
 crontab -r
 crontab mycron
